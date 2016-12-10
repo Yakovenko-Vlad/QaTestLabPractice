1cea4d219c498fa53128f4e7a87c98322cfdc21d
Yakovenko-Vlad <yakovenkovl12@gmail.com>
2016-12-10 21:40:56 +0200

    Full working version

--- src/main/java/Automation.java
+++ src/main/java/Automation.java
@@ -7,34 +7,31 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by Яковенко Влад on 07.12.2016.
+ * Created by Яковенко Влад
  */
 public class Automation {
-    private WebDriver driver;
-    private WebDriverWait wait;
+    public WebDriver driver;
+    public WebDriverWait wait;
     public Automation() {
         System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/drivers/chromedriver.exe");
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver,15);
     }
 
-    public void automation() throws InterruptedException {
-        openImagesPage();
-        scrollPage();
-        searchImages();
-        setDate();
-        openImage();
-        driver.quit();
-    }
-
-    private void openImagesPage(){
+    /**
+     * Открытие поисковой системы и переход в раздел изображений
+     */
+    public void openImagesPage(){
         driver.get("https://www.bing.com/");
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("sb_form_q"))));
         driver.findElement(By.id("scpl1")).click();
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("mainImage"))));
     }
 
-    private void scrollPage(){
+    /**
+     * Скрол страницы с изображениями
+     */
+    public void scrollPage(){
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
         for(int i=0;i<2;i++) {
@@ -48,7 +45,10 @@ public class Automation {
         }
     }
 
-    private void searchImages(){
+    /**
+     * Ввод слова без последней буквы и выбор нужного варианта
+     */
+    public void searchImages(){
         driver.findElement(By.id("sb_form_q")).sendKeys("automatio");
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("sa_ul"))));
         int count = driver.findElements(By.xpath(".//*[@id='sa_ul']/li")).size();//.//*[@id='sa_ul']/li[5]
@@ -61,7 +61,11 @@ public class Automation {
         }
     }
 
-    private void setDate() throws InterruptedException {
+    /**
+     * Изменение фильтра даты для отображения изображений
+     * @throws InterruptedException
+     */
+    public void setDate() throws InterruptedException {
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(".//*[@id='dg_c']/div[1]/div/div[1]/div/a/img"))));
         Thread.sleep(5000);
         driver.findElement(By.xpath(".//*[@id='ftrB']/ul/li[6]/span/span")).click();
@@ -70,23 +74,30 @@ public class Automation {
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(".//*[@id='dg_c']/div[1]/div/div[1]/div/a/img"))));
     }
 
-    private void openImage() {
+    /**
+     * Метод открывающий режим слайд-шоу, переключается между изображениями,
+     * переходит на новую вкладку с выбраным изображением
+     */
+    public void openImage() {
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         Actions builder = new Actions(driver);
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(".//*[@id='dg_c']/div[1]/div/div[1]/div/a/img"))));
         driver.findElement(By.xpath(".//*[@id='dg_c']/div/div/div[1]/div/a/img")).click();
         driver.switchTo().frame(driver.findElement(By.id("OverlayIFrame")));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(".//*[@id='iol_imw']/div[1]/span/span/img"))));
         wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("iol_navr"))));
         driver.findElement(By.id("iol_navr")).click();
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("iol_navl"))));
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(By.xpath(".//*[@id='iol_fsc']/div/a[2]")), "class","iol_fst iol_fsst"));
         driver.findElement(By.id("iol_navl")).click();
-        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(".//*[@id='iol_imw']/div[1]/span/span/img"))));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(".//*[@id='iol_imw']/div[1]/span/span/img"))));
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(By.xpath(".//*[@id='iol_fsc']/div/a[1]")), "class","iol_fst iol_fsst"));
         String imgUrl = driver.findElement(By.xpath(".//*[@id='iol_imw']/div[1]/span/span/img")).getAttribute("src");
         driver.findElement(By.xpath(".//*[@id='iol_imw']/div[1]/span/span/img")).click();
         for(String t:driver.getWindowHandles()){
             driver.switchTo().window(t);
         }
         wait.until(ExpectedConditions.urlToBe(imgUrl));
+        driver.quit();
     }
-
 }
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,41 +1,14 @@
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import javax.swing.*;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 /**
- * Created by Яковенко Влад on 02.12.2016.
+ * Created by Яковенко Влад
  */
 public class MainClass {
     public static void main(String[] args) throws InterruptedException {
         Automation object = new Automation();
-        object.automation();
-
-
-
-
-        /*Actions builder = new Actions(driver);
-        builder.clickAndHold(date).build().perform();
-        builder.release().build().perform();*/
-
-
-
-
-        //.//*[@id='mmComponent_images_1_list_6']/li[7]/div/div[1]/a/div/img
-        //.//*[@id='mmComponent_images_1_list_6']/li[11]/div/div[1]/a/div/img
-
-        //.//*[@id='mmComponent_images_1_list_6']/li[7]/div/div[1]/a/div/img
-        //.//*[@id='mmComponent_images_1_list_6']/li[12]/div/div[1]/a/div/img
-
-
-
-
-        //
+        object.openImagesPage();
+        object.scrollPage();
+        object.searchImages();
+        object.setDate();
+        object.openImage();
     }
 }
 
