8de748241f3eb3eb0750a4ca9a8f2b1e415e71dd
Yakovenko-Vlad <yakovenkovl12@gmail.com>
2016-12-25 19:30:35 +0200

    Final

---
+++ NodeStart.bat
@@ -0,0 +1,1 @@
+java -Dwebdriver.chrome.driver="drivers.chromedriver.exe" -Dwebdriver.gecko.driver="drivers.geckodriver.exe" -jar selenium-server-standalone-3.0.1.jar -role node -hub http://localhost:4444/grid/register -browser "browserName=firefox, maxInstances=1" -browser "browserName=chrome, maxInstances=1"
---
+++ SelendroidStart.bat
@@ -0,0 +1,1 @@
+java -Dfile.encoding=UTF-8 -cp "selendroid-grid-plugin-0.17.0.jar;selenium-server-standalone-3.0.1.jar" org.openqa.grid.selenium.GridLauncherV3 -capabilityMatcher io.selendroid.grid.SelendroidCapabilityMatcher -role hub -host 127.0.0.1 -port 4444
